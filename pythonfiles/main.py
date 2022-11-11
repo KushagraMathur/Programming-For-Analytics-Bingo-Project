7a51334bc585c94258eb248c184cc9c60150ea29
unknown <pranjal.rbl@gmail.com>
2022-11-11 20:59:39 +0000

    Added simulation logic

--- pythonfiles/main.py
+++ pythonfiles/main.py
@@ -1,13 +1,17 @@
 import UserInputClass
 import CardsGenerationClass
+import Simulation
 
 
 class CallingClass:
     def run(self):
         self.inputToValueDict = {'cards': 0, 'simulations': 0}
-        self.inputToValueDict = UserInputClass.UserInputClass().takingInputsFormUser(self.inputToValueDict)
+        self.inputToValueDict = UserInputClass.UserInputClass(
+        ).takingInputsFormUser(self.inputToValueDict)
         self.cardsArray = CardsGenerationClass.CardsGenerationClass(
         ).generateCards(self.inputToValueDict['cards'])
+        self.simulationSet = Simulation.Simulations().CountSimulations(
+            self.inputToValueDict['cards'], self.cardsArray)
 
 
 if __name__ == "__main__":
