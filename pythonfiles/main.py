32f6d6d66f3a213bcee78bce2c45ba3bb9122e5d
pranjal0204 <117543756+pranjal0204@users.noreply.github.com>
2022-11-15 15:50:00 +0000

    Update main.py

--- pythonfiles/main.py
+++ pythonfiles/main.py
@@ -3,18 +3,21 @@ import CardsGenerationClass
 import SimulationClass
 import PdfGenerationClass
 import GraphPlottingClass
-
+from collections import defaultdict, OrderedDict
 
 class CallingClass:
     def run(self):
-        self.inputToValueDict = {'cards': 0, 'simulations': 0}
+        self.inputToValueDict = {'cards': 0, 'simulations': 0, 'sizeOfCard': 0, 'numOfFreeCells': 0}
         self.inputToValueDict = UserInputClass.UserInputClass(
         ).takingInputsFromUser(self.inputToValueDict)
+        self.indicesOfFreeCellDict = defaultdict(list)        
+        self.indicesOfFreeCellDict = UserInputClass.UserInputClass(
+        ).takingIndicesForFreeCells(self.inputToValueDict['numOfFreeCells'], self.indicesOfFreeCellDict)        
         self.cardsArray = CardsGenerationClass.CardsGenerationClass(
-        ).generateCards(self.inputToValueDict['cards'])
+        ).generateCards(self.inputToValueDict['cards'], self.inputToValueDict['sizeOfCard'], self.indicesOfFreeCellDict)        
         PdfGenerationClass.PdfGenerationClass().CreatePdf(self.cardsArray)
         self.numOfWinnersDict = SimulationClass.SimulationsClass().CountSimulations(
-            self.inputToValueDict['cards'], self.cardsArray, self.inputToValueDict['simulations'])
+            self.inputToValueDict['cards'], self.cardsArray, self.inputToValueDict['simulations'], , self.inputToValueDict['sizeOfCard'])
         GraphPlottingClass.GraphPlottingClass().plotLineGraph(self.numOfWinnersDict)
 
 
