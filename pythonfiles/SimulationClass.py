c56fc29c18390a57168c1814bba37bb9013cb55d
pranjal0204 <117543756+pranjal0204@users.noreply.github.com>
2022-11-15 16:12:32 +0000

    Update SimulationClass.py

--- pythonfiles/SimulationClass.py
+++ pythonfiles/SimulationClass.py
@@ -6,25 +6,27 @@ import UserInputClass
 class SimulationsClass:
     '''
     @description
-    Method to check how many players reached bingo for 'n' number of simulations
+    Method to check how many players reached bingo after every number called
     @parameter
-    numberOfCards - The number of cards created by user.
-    cardsArray- An array equal to size of cards array.
-    numOfSim- The number of simulations entered by user.
+    countCardsSet- Contains the indices of cards that might have reached bingo after every number being called.
+    bingoCardsSet- Contains the indices of cards that have reached bingo.
+    testArray- An array equal to size of cards array. Contains one(1) for free cells and zeros(0) for others. 
+               Zeros are replaced by ones if a number is found in cards array.
+    sizeOfCard- The size of card specified by user.
     '''
 
-    def checkBingo(self, countCardsSet, bingoCardsSet, testArray):
+    def checkBingo(self, countCardsSet, bingoCardsSet, testArray, sizeOfCard):
         for index in countCardsSet:
-            if np.diagonal(self.testArray[index]).sum() == 5:
+            if np.diagonal(self.testArray[index]).sum() == sizeOfCard:
                 bingoCardsSet.add(index)
-            elif (np.diag(np.fliplr(self.testArray[index])).sum()) == 5:
+            elif (np.diag(np.fliplr(self.testArray[index])).sum()) == sizeOfCard:
                 bingoCardsSet.add(index)
             if index in bingoCardsSet:
                 continue
-            for row in range(0, 5):
-                if self.testArray[index, row].sum() == 5:
+            for row in range(0, sizeOfCard):
+                if self.testArray[index, row].sum() == sizeOfCard:
                     bingoCardsSet.add(index)
-                elif self.testArray[index, :, row].sum() == 5:
+                elif self.testArray[index, :, row].sum() == sizeOfCard:
                     bingoCardsSet.add(index)
 
     '''
@@ -34,15 +36,16 @@ class SimulationsClass:
     numberOfCards - The number of cards created by user.
     cardsArray- An array equal to size of cards array.
     numOfSim- The number of simulations entered by user.
+    sizeOfCard- The size of card specified by user.
     '''
 
-    def CountSimulations(self, numberOfCards, cardsArray, numOfSim):
+    def CountSimulations(self, numberOfCards, cardsArray, numOfSim, sizeOfCard):
         numOfWinnersDict = defaultdict(list)
         loopVariable = 1
         while loopVariable <= numOfSim:
-            self.bingoNumbers = np.arange(1, 76)
+            self.bingoNumbers = np.arange(1, sizeOfCard*sizeOfCard*3+1)
             np.random.shuffle(self.bingoNumbers)
-            self.testArray = np.zeros((numberOfCards, 5, 5))
+            self.testArray = np.zeros((numberOfCards, sizeOfCard, sizeOfCard))
             self.testArray[:, 2, 2] = 1
             self.bingoCardSet = set()
             countCardsSet = set()
@@ -52,8 +55,10 @@ class SimulationsClass:
                 self.testArray[cardsArray == number] = 1
                 countCardsSet.update(list((np.where(cardsArray == number))[0]))
                 self.checkBingo(
-                    countCardsSet, self.bingoCardSet, self.testArray)
+                    countCardsSet, self.bingoCardSet, self.testArray, sizeOfCard)
                 numOfWinnersDict[numbersCalled].append(len(self.bingoCardSet))
                 numbersCalled += 1
             loopVariable += 1
+        for key, value in numOfWinnersDict.items():
+            print(key, ' : ', value)
         return numOfWinnersDict
