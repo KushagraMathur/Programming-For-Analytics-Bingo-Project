7b15e09e5c253e5e0523acc9a304176621ff578d
unknown <kenttan4320@gmail.com>
2022-11-20 21:41:57 +0000

    Updating Input Logic

--- pythonfiles/PdfGenerationClass.py
+++ pythonfiles/PdfGenerationClass.py
@@ -1,6 +1,8 @@
 from fpdf import FPDF
 import numpy as np
-
+from PIL import Image
+import requests
+from io import BytesIO
 
 class PdfGenerationClass:
     '''
@@ -10,22 +12,24 @@ class PdfGenerationClass:
     cardsArray- Holds the cards to be added to pdf.
     '''
 
-    def CreatePdf(self, cardsArray):
+    def CreatePdf(self, cardsArray, sizeOfCard):
         pdf = FPDF()
-        pdf.set_left_margin(70)
-        pdf.set_top_margin(50)
         line_height = 10
-        col_width = pdf.epw / 8
+        col_width = pdf.w / (sizeOfCard * 1.2)
+        pdf.set_left_margin((pdf.w - sizeOfCard * col_width) / 2)
+        pdf.set_top_margin((pdf.h - (sizeOfCard * line_height + 10)) / 2)
         start = 1
+        response = requests.get(
+            'https://img.freepik.com/premium-vector/bingo-lottery-yellow-banner_100478-478.jpg?w=2000')
+        image = Image.open(BytesIO(response.content))
         for index in cardsArray:
             pdf.add_page()
             pdf.set_font("Courier", "BI", size=25)
             pdf.set_fill_color(r=138, g=43, b=226)
             pdf.set_text_color(r=252, g=252, b=252)
-            pdf.cell(82, 10, 'Bingo Card '+str(start), 0, 1, 'C', fill=1)
+            pdf.cell((sizeOfCard * col_width), 10, 'Bingo Card '+str(start), 0, 1, 'C', fill=1)
             pdf.set_font("Times", "B", size=15)
             pdf.set_text_color(r=0, g=0, b=0)
-            col_width = pdf.epw / 8
             start += 1
             for row in index:
                 textArray = np.array2string(
@@ -33,11 +37,14 @@ class PdfGenerationClass:
                 textArray = textArray[1:-1]
                 for numberString in textArray.split(','):
                     pdf.set_fill_color(r=252, g=100, b=150)
-                    if numberString == '-1.':
-                        pdf.multi_cell(col_width, line_height, "FREE", border=1, align='C',
-                                       ln=3, max_line_height=pdf.font_size, fill=True)
-                    else:
-                        pdf.multi_cell(col_width, line_height, numberString[:-1], border=1, align='C',
-                                       ln=3, max_line_height=pdf.font_size, fill=True)
+                    if numberString =="-1." or numberString == " -1.":
+                        xPos = pdf.get_x()
+                        yPos = pdf.get_y()
+                    pdf.multi_cell(col_width, line_height,
+                                   numberString[:-1], border=1, align='C',
+                                   ln=3, max_line_height=pdf.font_size, fill=True)
+                    if numberString =="-1." or numberString == " -1.":
+                        pdf.image(image, x=xPos, y=yPos,
+                                  w=col_width, h=line_height)
                 pdf.ln(line_height)
         pdf = pdf.output("Bingo Cards.pdf")
