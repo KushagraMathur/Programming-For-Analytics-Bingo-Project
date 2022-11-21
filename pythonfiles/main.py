3058f844090299cd79de7bf87851c35363beca3a
pranjal0204 <117543756+pranjal0204@users.noreply.github.com>
2022-11-21 22:40:25 +0000

    Update main.py

--- pythonfiles/main.py
+++ pythonfiles/main.py
@@ -3,21 +3,23 @@ import CardsGenerationClass
 import SimulationClass
 import PdfGenerationClass
 import GraphPlottingClass
-from collections import defaultdict, OrderedDict
+from collections import defaultdict
+
+# https://img.freepik.com/premium-vector/bingo-lottery-yellow-banner_100478-478.jpg?w=2000
 
 class CallingClass:
     def run(self):
-        self.inputToValueDict = {'cards': 0, 'simulations': 0, 'sizeOfCard': 0, 'numOfFreeCells': 0}
+        self.inputToValueDict = {'cards': 0, 'simulations': 0, 'sizeOfCardRow': 0, 'sizeOfCardCol': 0, 'lowerRangeOfCardNo': 0, 'upperRangeOfCardNo': 0, 'imageURL': 0, 'numbersCalledforHistogram': 0, 'numOfFreeCells': 0}
         self.inputToValueDict = UserInputClass.UserInputClass(
         ).takingInputsFromUser(self.inputToValueDict)
         self.indicesOfFreeCellDict = defaultdict(list)        
         self.indicesOfFreeCellDict = UserInputClass.UserInputClass(
-        ).takingIndicesForFreeCells(self.inputToValueDict['numOfFreeCells'], self.indicesOfFreeCellDict, self.inputToValueDict['sizeOfCard'])        
+        ).takingIndicesForFreeCells(self.inputToValueDict['numOfFreeCells'], self.indicesOfFreeCellDict, self.inputToValueDict['sizeOfCardRow'], self.inputToValueDict['sizeOfCardCol'])        
         self.cardsArray = CardsGenerationClass.CardsGenerationClass(
-        ).generateCards(self.inputToValueDict['cards'], self.inputToValueDict['sizeOfCard'], self.indicesOfFreeCellDict)        
-        PdfGenerationClass.PdfGenerationClass().CreatePdf(self.cardsArray, self.inputToValueDict['sizeOfCard'])
+        ).generateCards(self.inputToValueDict['cards'], self.inputToValueDict['sizeOfCardRow'], self.inputToValueDict['sizeOfCardCol'], self.indicesOfFreeCellDict, self.inputToValueDict['lowerRangeOfCardNo'], self.inputToValueDict['upperRangeOfCardNo'])        
+        PdfGenerationClass.PdfGenerationClass().CreatePdf(self.cardsArray, self.inputToValueDict['sizeOfCardRow'], self.inputToValueDict['sizeOfCardCol'], self.inputToValueDict['imageURL'])
         self.numOfWinnersDict = SimulationClass.SimulationsClass().CountSimulations(
-            self.inputToValueDict['cards'], self.cardsArray, self.inputToValueDict['simulations'], self.inputToValueDict['sizeOfCard'], self.indicesOfFreeCellDict)
+            self.inputToValueDict['cards'], self.cardsArray, self.inputToValueDict['simulations'], self.inputToValueDict['sizeOfCardRow'], self.inputToValueDict['sizeOfCardCol'], self.indicesOfFreeCellDict,  self.inputToValueDict['lowerRangeOfCardNo'], self.inputToValueDict['upperRangeOfCardNo'])
         GraphPlottingClass.GraphPlottingClass().plotLineGraph(self.numOfWinnersDict)
 
 
